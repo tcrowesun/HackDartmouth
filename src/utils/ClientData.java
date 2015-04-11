83425f42756185587fa362773388677034e9765b
Tyler Crowe <tcrowepolo@gmail.com>
2015-04-11 14:57:48 -0400

    Added Client Data
    util class in utils package

---
+++ src/utils/ClientData.java
@@ -0,0 +1,22 @@
+package utils;
+
+public class ClientData {
+
+	private final int x;
+	private final int y;
+	
+	public ClientData(int xCord, int yCord){
+		this.x = xCord;
+		this.y = yCord;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	
+}
